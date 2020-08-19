1f3cbebd4ebc5e984e507e3828cdd69d8ffa568e
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-19 09:35:39 -0400

    FEAT: Create componente AddCategory

---
+++ src/components/AddCategory.tsx
@@ -0,0 +1,29 @@
+import React, { useState, MouseEvent, Dispatch, SetStateAction } from 'react'
+
+interface AddCategoryProps {
+    setCategories: Function
+}
+
+export const AddCategory = (props: AddCategoryProps) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        inputValue.trim().length > 1 && props.setCategories( (categories: string[]) =>  [...categories, inputValue])
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input
+            type='text'
+            value={ inputValue }
+            onChange={ handleInput }
+            />
+        </form>
+    )
+}
--- src/components/GifExpertApp.tsx
+++ src/components/GifExpertApp.tsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
+import { AddCategory } from './AddCategory';
 
 const GifExpertApp = () => {
 
     const [categories, setcategories] = useState(['one punch', 'Samurai X', 'Dragon Ball']);
 
-    const handleAdd = () => {
-        setcategories([...categories, 'perro']);
-    }
-
     return(
         <>
             <h2>GifExpertApp</h2>
+            < AddCategory setCategories={ setcategories }/>
             <hr/>
-            <button onClick={handleAdd}>Agregar</button>
             <ol>
                 {
                     categories.map( (category) => <li key={category}>{ category }</li>)
