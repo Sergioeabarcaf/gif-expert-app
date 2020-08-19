ff770d48dd758442111a98f9ef628910d52e8259
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-19 15:43:59 -0400

    DEPLOY: deploy build app V0.0

---
+++ docs/precache-manifest.f3693323e99a1d18486fe2d0588bbe9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa402dd0c079c3d71874c3df4e2feb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2417723eac4ba9a84e4f",
+    "url": "/static/css/main.5bb245fb.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2417723eac4ba9a84e4f",
+    "url": "/static/js/main.7d336f84.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
