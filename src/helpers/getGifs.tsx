1ca010a0802a58bf5ddf2b46d7354035f03b4852
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-20 17:05:21 -0400

    REFACTOR: use environment variables

--- src/helpers/getGifs.tsx
+++ src/helpers/getGifs.tsx
@@ -12,8 +12,9 @@ interface GifsResponse {
 
 export const getGifs = async (category: string) => {
 
-  const limit = 10;
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=W3atpkF628WfcrLhyjGldHEGMTHLY76G&q=${encodeURI(category)}&limit=${limit}`;
+  const limit = process.env.REACT_APP_LIMIT;
+  const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }=${encodeURI(category)}&limit=${limit}`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
