e24f22c104159ec8decc3654dc51fa9191830e54
sergioeabarcaf <sergio.abarca@patagonia-it.com>
2020-08-20 16:37:35 -0400

    FEAT: Test in custom hook

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -2,15 +2,30 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('realizar pruebas sobre hook useFetchGifs', () => {
-  test('Debería retornar el estado incial', () => {
+  test('Debería retornar el estado incial', async() => {
 
     const category = 'Dragon Ball';
-    const { result } = renderHook( () =>  useFetchGifs(category));
+    const { result, waitForNextUpdate } = renderHook( () =>  useFetchGifs(category));
     const { data, loading } = result.current;
+
+    await waitForNextUpdate();
 
     expect( data ).toEqual( [] );
     expect( loading ).toBeTruthy();
     
-  })
+  });
+
+  test('debería retornar un array de imágenes y el loading en false ', async() => {
+
+    const category = 'Dragon Ball';
+    const { result, waitForNextUpdate } = renderHook( () =>  useFetchGifs(category));
+    const { data, loading } = result.current;
+
+    await waitForNextUpdate();
+
+    expect( data ).toEqual( [] );
+    expect( loading ).toBeTruthy();
+    
+  });
   
 });
